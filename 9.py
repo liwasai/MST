1c8af93ba754f74904d0ae74243e05a6e27daa85
liwasai <lcm472437013@qq.com>
2020-05-29 10:35:58 +0800

    第九天

---
+++ 9.py
@@ -0,0 +1,63 @@
+1. 下列哪个表达式在Python中是非法的？ B
+
+A. x = y = z = 1
+
+B. x = (y = z + 1)
+
+C. x, y = y, x
+
+D. x += y
+
+2. python my.py v1 v2 命令运行脚本，通过 from sys import argv如何获得v2的参数值? C
+
+A. argv[0]
+
+B. argv[1]
+
+C. argv[2]
+
+D. argv[3]
+
+3. 如何解释下面的执行结果? B
+
+print 1.2 - 1.0 == 0.2
+
+False
+
+A. Python的实现有错误
+
+B. 浮点数无法精确表示
+
+C. 布尔运算不能用于浮点数比较
+
+D. Python将非0数视为False
+
+4. 下列代码执行结果是什么? D
+
+x = 1
+
+def change(a):
+
+x += 1
+
+print x
+
+change(x)
+
+A. 1
+
+B. 2
+
+C. 3
+
+D. 报错
+
+5. 下列哪种类型是Python的映射类型？ D
+
+A. str
+
+B. list
+
+C. tuple
+
+D. dict
